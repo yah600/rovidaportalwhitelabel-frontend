33aa3a748d19c4724cbe2e2979e09f4cc3043fd6
[dyad] <git@dyad.sh>
2025-10-01 13:09:04 -0400

    [dyad] Implemented the OGL Aurora component as the animated background for the application - wrote 7 file(s), added ogl package(s)

--- src/components/layout/AppShell.tsx
+++ src/components/layout/AppShell.tsx
@@ -4,6 +4,7 @@ import Topbar from './Topbar';
 import Sidebar from './Sidebar';
 import MobileBottomNav from './MobileBottomNav';
 import { MadeWithDyad } from '@/components/made-with-dyad';
+import Aurora from '@/components/Aurora'; // Import Aurora component
 
 interface AppShellProps {
   // children: React.ReactNode; // No longer needed with Outlet
@@ -11,7 +12,13 @@ interface AppShellProps {
 
 const AppShell = (/* { children }: AppShellProps */) => { // Remove children prop
   return (
-    <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
+    <div className="relative grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
+      <Aurora
+        colorStops={["#183747", "#C4972E", "#7C8D89"]} // Using Rovida palette colors
+        blend={0.5}
+        amplitude={1.0}
+        speed={0.5}
+      />
       <Sidebar />
       <div className="flex flex-col">
         <Topbar />
