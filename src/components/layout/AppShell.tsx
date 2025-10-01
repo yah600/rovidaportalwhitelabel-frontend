f2ffdcca3c031750475bb4ce2059e9d9de114e6f
[dyad] <git@dyad.sh>
2025-10-01 12:10:02 -0400

    [dyad] Fixed routing and content rendering by using React Router's Outlet in AppShell - wrote 2 file(s)

--- src/components/layout/AppShell.tsx
+++ src/components/layout/AppShell.tsx
@@ -1,21 +1,22 @@
 import React from 'react';
+import { Outlet } from 'react-router-dom'; // Import Outlet
 import Topbar from './Topbar';
 import Sidebar from './Sidebar';
 import MobileBottomNav from './MobileBottomNav';
 import { MadeWithDyad } from '@/components/made-with-dyad';
 
 interface AppShellProps {
-  children: React.ReactNode;
+  // children: React.ReactNode; // No longer needed with Outlet
 }
 
-const AppShell = ({ children }: AppShellProps) => {
+const AppShell = (/* { children }: AppShellProps */) => { // Remove children prop
   return (
     <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
       <Sidebar />
       <div className="flex flex-col">
         <Topbar />
         <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6">
-          {children}
+          <Outlet /> {/* Render child routes here */}
         </main>
         <MadeWithDyad />
       </div>
