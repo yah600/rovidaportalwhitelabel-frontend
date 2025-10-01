28208bfefb93dcebcce3a5aab41dc12c0d40e6d3
[dyad] <git@dyad.sh>
2025-10-01 16:59:37 -0400

    [dyad] Resolved React Router future flag warnings - wrote 1 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -76,7 +76,12 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter
+        future={{
+          v7_startTransition: true,
+          v7_relativeSplatPath: true,
+        }}
+      >
         <Routes>
           <Route path="/" element={<Index />} />
           {/* Authentication Routes */}
